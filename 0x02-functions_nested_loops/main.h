c9978d95a7b7dba30dc70bb889d749fd2df9084f
kaymacharia <kaymacharia@gmail.com>
2023-03-21 04:43:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,6 @@
-#ifndef main_h
-#define main_h
-
-Prototype: void print_alphabet(void);
-Prototype: void print_alphabet_x10(void);
-Prototype: int _islower(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
@@ -12,5 +9,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
