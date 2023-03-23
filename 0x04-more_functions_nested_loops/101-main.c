940a87600d47e2622eb925a06e3ba021d02a6c2f
kaymacharia <kaymacharia@gmail.com>
2023-03-23 13:16:25 -0700

    updated

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	print_number(98);
-	putchar('\n');
-	print_number(402);
-	putchar('\n');
-	print_number(1024);
-	putchar('\n');
-	print_number(0);
-	putchar('\n');
-	print_number(-98);
-	putchar('\n');
-	return (0);
+print_number(98);
+putchar('\n');
+print_number(402);
+putchar('\n');
+print_number(1024);
+putchar('\n');
+print_number(0);
+putchar('\n');
+print_number(-98);
+putchar('\n');
+return (0);
 }
