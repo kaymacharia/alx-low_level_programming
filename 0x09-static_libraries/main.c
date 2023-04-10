08380f7a48c1988bdebfefa53969cdae8198da7f
kaymacharia <kaymacharia@gmail.com>
2023-04-10 10:24:58 -0700

    main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,9 @@
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts
+		(
+		 "\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick"
+		);
 	return (0);
 }
