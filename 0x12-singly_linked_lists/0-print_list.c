b46934b0db01c1fbd4a4ddf1da2d1ab5e73c5c55
kaymacharia <kaymachariaa@gmail.com>
2023-04-28 02:49:14 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,27 +6,23 @@
 * @param h: pointer to the beginning of list
 * @ return number of linked list nodes
 */
-size_t print_list( const list_t *h)
+size_t print_list(const list_t *h)
 {
-size_t s = 0;
+   int count = 0;
 
-// Cross every element of the linked list
-while (h)
-{
-// Print valid length node The contents of string
-if (!h ->str)
-{
-printf("[ 0] (None) \N ");
-}
-else
-{
-printf("[%u] %s\n", h->len, h->str );
-}
 
-// Go to next node
-h = h->next;
-seconds ++;
+   while (h)
+   {
+       if (h->str == NULL)
+       {
+           printf("[0] (nil)\n");
+       }
+       else
+       {
+       printf("[%d] %s\n", h->len, h->str);
+       }
+       count++;
+       h = h->next;
+   }
+   return (count);
 }
-
-// Returns the number of nodes in the linked list
-Returns s;
