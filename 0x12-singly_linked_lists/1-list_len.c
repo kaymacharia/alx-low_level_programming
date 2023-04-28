c4e78187ddbd27ccaf1017a80f2c9d328a3fb021
kaymacharia <kaymachariaa@gmail.com>
2023-04-28 03:18:12 -0700

    updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,9 @@
 #include "lists.h"
 
 /**
- *returns the number of linked list 
+ *list_len-returns the number of linked list
+ *@h-pointer to the linked list
+ *Return- return thr number of elements in h
  */
 size_t list_len(const list_t *h)
 {
