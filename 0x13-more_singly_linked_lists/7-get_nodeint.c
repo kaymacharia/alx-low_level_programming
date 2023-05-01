61e774851a8033d825e6f163041d4ae939a3f05d
kaymacharia <kaymachariaa@gmail.com>
2023-05-01 14:56:38 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
- * @head: pointer to the head of the linked list
+ * @kay: pointer to the head of the linked list
  * @index: index of the node to return
  *
  * Return: pointer to the nth node, or NULL if it doesn't exist
