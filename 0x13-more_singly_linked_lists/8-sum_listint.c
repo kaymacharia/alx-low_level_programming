59bd97ff160d40e3627dc6579a6748ffb9cd22a4
kaymacharia <kaymachariaa@gmail.com>
2023-05-01 15:05:42 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,17 +2,16 @@
 /**
  * sum_listint - returns the sum of all the data (n)
  * of a listint_t linked list.
- * @head: pointer to the head node of the linked list
+ * @kay: pointer to the head node of the linked list
  *
  * Return: the sum of all the data (n)
  * of the linked list or 0 if list is empty.
  */
 int sum_listint(listint_t *kay)
 {
-int sum = 0;
-
+int sum=0;
 while (kay != NULL)
-}
+{
 sum += kay->n;
 kay = kay->next;
 }
