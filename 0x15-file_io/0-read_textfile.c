b791fb9c2f65a417153c01f278840c2f022df881
kaymacharia <kaymachariaa@gmail.com>
2023-05-08 17:02:10 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-*read_textfile - Read text file to print to STDOUT.
+*read_textfile - Read text file to Read text file print to STDOUT.print to STDOUT.
 *@filename: the name of the filethat is to be read
 *@letters: the number of characters to be read from the file
 *returns: the number of bytes successfully read and written,
@@ -11,28 +11,28 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t op, rd, wr;
-	char *buffer;
+        ssize_t op, rd, wr;
+        char *buffer;
 
-	if (filename == NULL)
-		return (0);
+        if (filename == NULL)
+                return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+        buffer = malloc(sizeof(char) * letters);
+        if (buffer == NULL)
+                return (0);
 
-	op = open(filename, O_RDONLY);
-	rd = read(op, buffer, letters);
-	wr = write(STDOUT_FILENO, buffer, rd);
+        op = open(filename, O_RDONLY);
+        rd = read(op, buffer, letters);
+        wr = write(STDOUT_FILENO, buffer, rd);
 
-	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
-	{
-		free(buffer);
-		return (0);
-	}
+        if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+        {
+                free(buffer);
+                return (0);
+        }
 
-	free(buffer);
-	close(op);
+        free(buffer);
+        close(op);
 
-	return (wr);
+        return (wr);
 }
