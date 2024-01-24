93ce77ed42873790749d5f07857a9cd7c33f7bb8
kaymacharia <kaymachariaa@gmail.com>
2024-01-24 03:34:08 -0800

    1

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""File representing island"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of an island.
+    """
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return (perimeter)
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "m.h"
+
+/**
+ * main - THE ENTRY POINT
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	print_holberton();
+	return (EXIT_SUCCESS);
+}
+/* School */
